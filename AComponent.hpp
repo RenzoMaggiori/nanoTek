26bc962419bcf5054631687dc224c2260333f66d
Renzo Maggiori <renzo.maggiori@epitech.eu>
2024-02-02 17:41:46 +0100

    [add] interface and abstract

---
+++ AComponent.hpp
@@ -0,0 +1,14 @@
+#ifndef ACOMPONENT_HPP
+#define ACOMPONENT_HPP
+
+#include "IComponent.hpp"
+
+class AComponent: public IComponent
+{
+    private:
+    public:
+        AComponent();
+        ~AComponent();
+};
+
+#endif
---
+++ IComponent.hpp
@@ -0,0 +1,14 @@
+#ifndef ICOMPONENT_HPP
+#define ICOMPONENT_HPP
+
+class IComponent
+{
+    private:
+        /* data */
+    public:
+        IComponent(/* args */);
+        ~IComponent();
+};
+
+
+#endif
