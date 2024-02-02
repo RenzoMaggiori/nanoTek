8f81c0e2415cadcbd40f3cd3f98d81852e1edd80
Renzo Maggiori <renzo.maggiori@epitech.eu>
2024-02-02 17:54:07 +0100

    [add] and & true component

---
+++ AndComponent.hpp
@@ -0,0 +1,15 @@
+#ifndef ANDCOMPONENT_HPP
+#define ANDCOMPONENT_HPP
+
+#include "AComponent.hpp"
+
+class AndComponent: public AComponent
+{
+    private:
+        /* data */
+    public:
+        AndComponent(/* args */);
+        ~AndComponent();
+};
+
+#endif
---
+++ TrueComponent.hpp
@@ -0,0 +1,14 @@
+#ifndef TRUECOMPONENT_HPP
+#define TRUECOMPONENT_HPP
+
+#include "AComponent.hpp"
+
+class TrueComponent: public AComponent
+{
+    private:
+    public:
+        TrueComponent(/* args */);
+        ~TrueComponent();
+};
+
+#endif
