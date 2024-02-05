4b3be26cb4b8afbfddf865ab6a7d7f16356804a0
Gonzalo <gonzalo.larroya-martinez@epitech.eu>
2024-02-05 10:40:06 +0100

    [add] Xor component

---
+++ components/XorComponent.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2024
+** nanoTek
+** File description:
+** XorComponent
+*/
+
+#ifndef XORCOMPONENT_HPP_
+#define XORCOMPONENT_HPP_
+
+#include "../AComponent.hpp"
+
+namespace nts {
+    class XorComponent: public AComponent{
+        public:
+            XorComponent();
+            pinType getPinType(std::size_t pin) override;
+            void updateOutputPin() override;
+        protected:
+        private:
+    };
+}
+
+#endif /* !XORCOMPONENT_HPP_ */
